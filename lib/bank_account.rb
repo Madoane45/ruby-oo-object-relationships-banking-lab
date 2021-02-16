2c3e95eb9102cbb059b77dc5b54e5dc8811db30d
Madoane45 <Madoane45@gmail.com>
2021-02-16 17:48:32 -0500

    commit tree 56d6b77aa31bd43b646d8e09bac108ec981c0cee

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
+    attr_accessor :balance, :status
+    attr_reader :name
+
+    def initialize (name)
+        @name = name
+        @status = "open"
+        @balance = 1000
+    end
+
+    def deposit(amount)
+        self.balance += amount
+
+    end
+    
+    def display_balance
+        "Your balance is $#{balance}."
+    end
+
+    def valid?
+        if balance > 0 
+            status == "open"
+        else
+            status == "closed"            
+        end
+    end
+
+    def close_account
+        self.status = "closed"
+    end
+            
+
+
 
 end
